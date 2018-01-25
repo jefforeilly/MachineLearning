56496bf5e079b6e1d9e61f5ae5a980cf33e537b8
vyzyv <vyz@protonmail.com>
2018-01-25 18:34:14 +0100

    Add documentation and comments

--- code/unsupervised_algorithms/KMeans/init_methods.py
+++ code/unsupervised_algorithms/KMeans/init_methods.py
@@ -1,21 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Methods for KMeans initialization."""
+
 import multiprocessing
 
 import numpy as np
 
 
 def k_means_plus_plus(X, n_clusters: int):
+    """K-Means++ cluster initialization method.
 
+       Computes clusters based on distance between data points.
+
+       Algorithm:
+           1. Choose random data point as initial cluster.
+           2. Remove lastly chosen datapoint from dataset.
+           3. Calculate distance between current cluster and all remaining data
+           points (using :math: `|c - x|`, where c is cluster and x is data
+           point).
+           4. Assign probabilities to each cluster based on the distance
+           :math: `p(x) = |c-x|^2` and create weighted probability dividing by
+           sum of all distances.
+           5. Randomly choose new cluster based on weighted probability array
+           and append it to clusters list.
+           6. Repeat steps 2-5 until all clusters are assigned
+
+       Parameters
+       ----------
+       X : np.ndarray
+           Dataset array containing features (points) from which clusters are
+           chosen. Shape (n_examples, n_features)
+
+       n_clusters: int
+           Number of clusters to create
+
+       Returns
+       -------
+       np.ndarray
+           Matrix containg cluster points. Shape (n_clusters, n_features)
+    """
+
+    # Choose random row as initial centroid
     random_row = np.random.randint(0, X.shape[0])
     centroids = [X[random_row]]
+    # Remove it from array
     cleared_array = np.delete(X, random_row, axis=0)
 
+    # First cluster is assigned so range decreases by one
     for i in range(n_clusters - 1):
+        # Calculate distance between cluster and all points
         squared_distances = np.linalg.norm(
             centroids[i] - cleared_array, axis=1)**2
+        # Create weighted probability dividing by sym
         probabilities = squared_distances / np.sum(squared_distances)
 
+        # Randomly select new centroid based on weighted probability array
         random_row = np.random.choice(len(probabilities), p=probabilities)
 
+        # Add new centroid and remove it from the dataset
         centroids.append(X[random_row])
         cleared_array = np.delete(X, random_row, axis=0)
+
     return np.array(centroids)
--- code/unsupervised_algorithms/KMeans/k_means.py
+++ code/unsupervised_algorithms/KMeans/k_means.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Implementation of K-Means algorithm."""
+
 import multiprocessing
 import sys
 
@@ -8,14 +12,42 @@ from init_methods import k_means_plus_plus
 
 
 class DistanceFromExamples:
+    """Convenience class for easier multithreaded calculation of distance
+    between clusters and data points."""
+
     def __init__(self, X):
+        """Packs X data points for further use (less calls).
+
+           Parameters
+           ----------
+           X : np.array or pd.DataFrame
+               Dataset containing points to be clustered. Shape (n_examples,
+               n_features)
+
+        """
         self.X = X
 
-    def __call__(self, a):
-        return np.linalg.norm(self.X - a, axis=1)
+    def __call__(self, c):
+        """Returns distance between data points in X and cluster c.
+
+           Parameters
+           ----------
+           c : np.array
+               Vector containing features of given cluster.
+               Shape (n_features, )
+
+           Returns
+           -------
+           np.array
+               Vector containing distances of each data point from cluster.
+               Shape (n_examples, )
+        """
+        return np.linalg.norm(self.X - c, axis=1)
 
 
 class KMeans:
+    """KMeans (short summary)."""
+
     def __init__(self,
                  *,
                  n_clusters: int = 8,
@@ -23,16 +55,77 @@ class KMeans:
                  tolerance: int = 1e-4,
                  max_iterations: int = 300,
                  n_jobs: int = None):
+        """Setup for K-Means algorithm.
+
+           Parameters
+           ----------
+           n_clusters : int, optional
+               Number of clusters to create for KMeans (default is 8)
+           initialization : str or array, optional
+               Method of initialization, one of three strings should be
+               provided:
+                   a) 'k-means++': KMeans++ initialization algorithm,
+                   see init_methods.py (default choice)
+                   b) 'random': randomly initialize clusters
+                   c) numpy array: array of shape (n_clusters, n_features)
+                   containing centroids for algorithm
+           tolerance : int, optional
+               If distance between previous and current centroids is smaller
+               than tolerance, finish training (default: 1e-4)
+           max_iterations : int, optional
+               Maximum number of iterations to perform during training (update
+               clusters at most max_iterations times) (default: 300)
+           n_jobs: int, optional
+               Number of parallel jobs used for cluster calculations (default:
+               number of cores).
+
+               Each cluster may be computed in parallel, so maximum can be set
+               by number of clusters (e.g. 3 clusters with n_jobs=8 would still
+               give parallelism equal to 3)
+
+        """
         self.n_clusters = n_clusters
         self.initialization = initialization
         self.max_iterations = max_iterations
 
         self.tolerance = tolerance
-        self.n_jobs = n_jobs
+        if self.n_jobs = None:
+            self.n_jobs = n_jobs if n_jobs <= n_clusters else n_clusters
+        else:
+            self.n_jobs - n_jobs
         self.labels = None
         self.cluster_centers = None
 
     def fit(self, X):
+        """Trains classifier on given dataset.
+
+           Algorithm:
+               1. Create centroids using one of the methods from __init__.
+               2. Assign each datapoint to the nearest cluster based on the
+               distance :math: `D(x, c) = |c - x|`, where c is current cluster
+               center and x is a data point.
+               3. Calculate new centroids taking the mean of features from
+               datapoints assigned to certain cluster.
+               4. Repeat steps 2-3 until convergence or max_iterations is
+               surpassed.
+
+           Parameters
+           ----------
+           X : np.array or pd.DataFrame
+               Dataset containing points to be clustered. Shape (n_examples,
+               n_features)
+
+           Throws
+           -------
+           ValueError
+               ValueError with message 'Initialization array of wrong shape' if
+               initialization parameter during object creation is np.array and
+               is of wrong shape
+           ValueError
+               ValueError with message 'Unknown initialization scheme' if
+               initialization is incorrect
+        """
+        # If X is pandas DataFrame transform it to numpy array
         X = X.values if isinstance(X, pd.DataFrame) else X
 
         # PARSE INITIALIZATION METHOD
@@ -47,34 +140,80 @@ class KMeans:
         else:
             raise ValueError('Unknown initialization scheme')
 
+        # Create pool of processes to use for training
         with multiprocessing.Pool(processes=self.n_jobs) as pool:
             for _ in range(self.max_iterations):
+                # Calculate distance from cluster for each data point
                 distances_from_clusters = np.array(
                     pool.map(DistanceFromExamples(X), self.cluster_centers))
 
+                # Assign each data point to cluster based on minimum distance
                 examples_clustered = np.argmin(distances_from_clusters, axis=0)
+
+                # Create mask with indices for each cluster, e.q. cluster one has
+                # mask [1,0,0,1,0] which means elements [0,4] will be chosen from
+                # the dataset for it.
+
                 masks = np.array([
                     np.equal(examples_clustered, cluster)
                     for cluster in np.unique(examples_clustered)
                 ])
+
+                # Slice array with the masks and calculate mean for all
+                # datapoints assigned to given cluster. Effectively creates new
+                # cluster centroids
                 new_cluster_centers = np.array(
                     [np.mean(X[mask], axis=0) for mask in masks])
 
+                # Calculate distance between previous and current cluster
                 iteration_distance = np.linalg.norm(
                     self.cluster_centers - new_cluster_centers)
 
+                # If smaller than tolerance, eng algorithm
                 if iteration_distance < self.tolerance:
                     return
 
                 self.cluster_centers = new_cluster_centers
 
     def predict(self, X):
+        """Predict clusters for given dataset.
+
+           Chooses cluster based on minimum distance between data point and
+           forementioned.
+
+           Parameters
+           ----------
+           X : np.array or pd.DataFrame
+               Dataset containing points to be clustered. Shape (n_examples,
+               n_features)
+
+           Returns
+           -------
+           np.array
+               Array containing predicted clusters. Shape (n_examples,)
+        """
+        X = X.values if isinstance(X, pd.DataFrame) else X
+        # Calculate disance between clusters and every data point
         distances_from_clusters = np.array(
             multiprocessing.Pool(processes=8).map(
                 DistanceFromExamples(X), self.cluster_centers))
 
+        # Choose the smallest distance
         return np.argmin(distances_from_clusters, axis=0)
 
     def fit_predict(self, X):
+        """Convenience methods calling fit and predict afterwards.
+
+           Parameters
+           ----------
+           X : np.array or pd.DataFrame
+               Dataset containing points to be clustered. Shape (n_examples,
+               n_features)
+
+           Returns
+           -------
+           np.array
+               Array containing predicted clusters. Shape (n_examples,)
+        """
         self.fit(X)
         return self.predict(X)
